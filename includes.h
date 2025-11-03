3729b8c659af37e532ea0ce89aa1344e74edb2c5
Caleb Blackler <cblackler19@gmail.com>
2025-11-03 17:50:46 +1300

    Moved #include from main.c to includes.h

---
+++ includes.h
@@ -0,0 +1,10 @@
+// includes for main.c
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
