d11603d452eb3e99ab032328b8c8962fa748a1b9
Caleb Blackler <cblackler19@gmail.com>
2025-11-03 17:51:16 +1300

    Moved #define from main.c to defines.h

---
+++ defines.h
@@ -0,0 +1,14 @@
+// define ansi codes
+#define CYAN      "\033[0;36m"
+#define GREEN     "\033[0;32m"
+#define RED       "\033[0;31m"
+#define YELLOW    "\033[0;33m"
+#define DEF       "\033[0m"
+#define CLR_CONS  "\e[1;1H\e[2J"
+
+// define memory addresses
+#define BOLTS_ADDR      ((uintptr_t)0x300969CA0ULL)
+#define BOMB_GLOVE_ADDR ((uintptr_t)0x30096C0D7ULL)
+
+// misc defines
+#define TARGET "rpcs3.exe"
